d695f0a36d2dc16d0225ebd5ab96c7cb9d1eb229
Edward Shin <shyunuya@gmail.com>
2018-07-05 17:58:49 +0900

    Practice

--- src/router/main.js
+++ src/router/main.js
@@ -11,6 +11,7 @@ module.exports = function(app,fs)
         fs.readFile(__dirname + '/../data/' + "user.json", 'utf8', function(err, data){
             console.log(data);
             res.end(data);
+
         });
     })
 
@@ -18,6 +19,32 @@ module.exports = function(app,fs)
         fs.readFile(__dirname+'/../data/user.json', 'utf8',function(err,data){
             var users = JSON.parse(data);
             res.json(users[req.params.username]);
+
         })
+    })
+
+    app.post('/addUser/:username', function(req,res){
+        var result = {};
+        var username = req.params.username;
+
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request mofos";
+            res.json(result);
+            return;
+        }
+
+        // Load data & check duplication
+        fs.readFile(__dirname + "/../data/user.json",'utf8',function(err,data){
+            var users = JSON.parse(data);
+            if(users[username]) {
+                //Duplication found
+                result["success"] = 0;
+                result["error"] = "duplicate mofo";
+                res.json(result);
+                return;
+            }
+        })
+
     })
 }
